e90caa5fdf4424149f3ee03e214e8848affe9386
fengwenping <244160190@163.com>
2018-08-16 18:18:00 +0800

    项目初始化

---
+++ src/main/java/org/jfteam/JfteamSpringBootAppApplication.java
@@ -0,0 +1,12 @@
+package org.jfteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfteamSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfteamSpringBootAppApplication.class, args);
+	}
+}
---
+++ src/main/java/org/jfteam/domain/User.java
@@ -0,0 +1,19 @@
+package org.jfteam.domain;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: fengwenping
+ * Date: 2018-08-16
+ * Time: 下午5:57
+ */
+public class User implements Serializable {
+
+    private Integer id;
+
+    private String name;
+
+    private int age;
+}
